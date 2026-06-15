a581a27f31662d9bd893166647b04e87e9292ca7
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-06-15 11:41:08 +0530

    add store persistance

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,6 +2,7 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../lib/store/store";
+import { setInitialCartItems } from "@/lib/store/features/cart/cartSlice";
 
 export default function StoreProvider({
   children,
@@ -13,6 +14,19 @@ export default function StoreProvider({
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
+    //todo: set initial data from local storage
+    const localStorageAvailable =
+      typeof window != "undefined" && window.localStorage;
+    if (localStorageAvailable) {
+      const cartItems = window.localStorage.getItem("cartItems");
+      try {
+        const parsedItems = JSON.parse(cartItems as string);
+        // eslint-disable-next-line react-hooks/refs
+        storeRef.current.dispatch(setInitialCartItems(parsedItems));
+      } catch (err) {
+        console.error(err);
+      }
+    }
   }
 
   // eslint-disable-next-line react-hooks/refs
