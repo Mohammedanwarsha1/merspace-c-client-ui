e4de5f280c0796a78147d4953b8ccabbf50895e7
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-06-16 19:48:27 +0530

    fix storeProvider .push problem

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useRef } from "react";
+
+import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../lib/store/store";
 import { setInitialCartItems } from "@/lib/store/features/cart/cartSlice";
@@ -10,25 +11,29 @@ export default function StoreProvider({
   children: React.ReactNode;
 }) {
   const storeRef = useRef<AppStore | null>(null);
+
+  // Create store only once
   // eslint-disable-next-line react-hooks/refs
   if (!storeRef.current) {
-    // Create the store instance the first time this renders
     storeRef.current = makeStore();
-    //todo: set initial data from local storage
-    const localStorageAvailable =
-      typeof window != "undefined" && window.localStorage;
-    if (localStorageAvailable) {
-      const cartItems = window.localStorage.getItem("cartItems");
-      try {
-        const parsedItems = JSON.parse(cartItems as string);
-        // eslint-disable-next-line react-hooks/refs
-        storeRef.current.dispatch(setInitialCartItems(parsedItems));
-      } catch (err) {
-        console.error(err);
-      }
-    }
   }
 
-  // eslint-disable-next-line react-hooks/refs
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  useEffect(() => {
+    try {
+      const cartItems = localStorage.getItem("cartItems");
+
+      if (cartItems) {
+        const parsedItems = JSON.parse(cartItems);
+
+        storeRef.current?.dispatch(setInitialCartItems(parsedItems));
+      }
+    } catch (err) {
+      console.error("Failed to load cart items:", err);
+    }
+  }, []);
+
+  return (
+    // eslint-disable-next-line react-hooks/refs
+    <Provider store={storeRef.current}>{children}</Provider>
+  );
 }
