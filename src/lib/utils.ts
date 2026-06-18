7196820277da80f9078fba0ac5f6da96e709eaf8
Mohammedanwarsha1 <mohammedanwarsha2019@gmail.com>
2026-06-18 10:24:13 +0530

    get from minimum price for product card

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import CryptoJS from "crypto-js";
 import { twMerge } from "tailwind-merge";
 import { CartItem } from "./store/features/cart/cartSlice";
+import { Product } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,4 +12,18 @@ export function hashTheItem(payload: CartItem): string {
   const jsonString = JSON.stringify({ ...payload, qty: undefined });
   const hash = CryptoJS.SHA256(jsonString).toString();
   return hash;
+}
+
+export function getFromPrice(product: Product): number {
+  const basePrice = Object.entries(product.priceConfiguration)
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .filter(([key, value]) => {
+      return value.priceType === "base";
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .reduce((acc, [key, value]) => {
+      const smallestPrice = Math.min(...Object.values(value.availableOptions));
+      return acc + smallestPrice;
+    }, 0);
+  return basePrice;
 }
